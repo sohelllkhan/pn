87c090d50d831638c0c2971c74095db2bc332f7d
Sohel Khan <sohelkhan323526@gmail.com>
2025-11-12 17:45:13 +0600

    Update final4.py

--- final4.py
+++ final4.py
@@ -6,6 +6,14 @@ import aiohttp
 import io, json, os, re
 
 # ---------------- CONFIG ---------------- #
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads .env file
+TOKEN = os.environ.get("DISCORD_TOKEN")
+
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
 HASH_DB_FILE = "hash_db.json"
@@ -183,6 +191,7 @@ async def on_ready():
     print("Context menus synced successfully!")
 
 bot.run(TOKEN)
+
 
 
 
